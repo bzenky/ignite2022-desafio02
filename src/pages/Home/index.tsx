76edbfae9d8b152ba0671f36780310513d226d6d
Zenky <bzenky@gmail.com>
2022-08-09 19:14:46 -0300

    feat: hero home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,51 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react';
+import coffeeDeliverySplash from '../../assets/coffeeDeliverySplash.png';
+import { Benefit, BenefitContainer, BenefitIconWrapper, BenefitsContainer, HomeContainer, SplashContainer, SubTitle, Title, TitleContainer } from './styles';
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <HomeContainer>
+      <SplashContainer>
+        <TitleContainer>
+          <Title>Encontre o café perfeito para qualquer hora do dia</Title>
+          <SubTitle>Com o Coffee Delivery você recebe seu café onde estiver a qualquer hora</SubTitle>
+
+          <BenefitsContainer>
+            <BenefitContainer>
+              <Benefit>
+                <BenefitIconWrapper backgroundColor='#C47F17'>
+                  <ShoppingCart size={16} weight="fill" color="white" />
+                </BenefitIconWrapper>
+                Compra simples e segura
+              </Benefit>
+
+              <Benefit>
+                <BenefitIconWrapper backgroundColor='#DBAC2C'>
+                  <Timer size={16} weight="fill" color="white" />
+                </BenefitIconWrapper>
+                Entrega rápida e rastreada
+              </Benefit>
+            </BenefitContainer>
+
+              <BenefitContainer>
+                <Benefit>
+                  <BenefitIconWrapper backgroundColor='#574F4D'>
+                    <Package size={16} weight="fill" color="white" />
+                  </BenefitIconWrapper>
+                  Embalagem mantém o café intacto
+                </Benefit>
+                <Benefit>
+                  <BenefitIconWrapper backgroundColor='#8047F8'>
+                    <Coffee size={16} weight="fill" color="white" />
+                  </BenefitIconWrapper>
+                  O café chega fresquinho até você
+                </Benefit>
+              </BenefitContainer>
+          </BenefitsContainer>
+        </TitleContainer>
+
+        <img src={coffeeDeliverySplash} alt="" />
+      </SplashContainer>
+    </HomeContainer>
+  )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+interface BenefitIconProps {
+  backgroundColor: string
+}
+
+export const HomeContainer = styled.div``
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  line-height: 1.3;
+  `
+
+export const SplashContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 56px;
+  padding: 6rem 0;
+  z-index: 5;
+`
+
+export const Title = styled.h1`
+  font-family: 'Baloo 2', cursive;
+  font-size: 3rem;
+  font-weight: 800;
+  color: ${({theme}) => theme['gray-800']};
+`
+
+export const SubTitle = styled.span`
+  font-size: 1.25rem;
+  color: ${({theme}) => theme['gray-700']};
+`
+
+export const BenefitsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 40px;
+`
+
+export const BenefitContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
+export const BenefitIconWrapper = styled.div<BenefitIconProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({backgroundColor}) => backgroundColor};
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+`
+
+export const Benefit = styled.span`
+  display: flex;
+  gap: 12px;
+  align-items: center;
+  font-size: 1rem;
+  color: ${({theme}) => theme['gray-600']};
+`
