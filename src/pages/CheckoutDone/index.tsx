7c17e37304398a00b7aa7fd64a5462deff32363a
Zenky <bzenky@gmail.com>
2022-08-21 20:28:11 -0300

    feat: static checkout-done page

--- src/pages/CheckoutDone/index.tsx
+++ src/pages/CheckoutDone/index.tsx
@@ -1,3 +1,62 @@
-export function CheckoutDone () {
-  return <h1>CheckoutDone</h1>
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
+import {
+  CheckoutDoneContainer,
+  CheckoutDoneMain,
+  CheckoutOrderIconWrapper,
+  CheckoutOrderInfoTypeWrapper,
+  CheckoutOrderInfoWrapper,
+  CheckoutOrderSpan,
+  CheckoutOrderSpanWrapper,
+  CheckoutSubtitle,
+  CheckoutTitle
+} from "./styles";
+
+import checkoutDone from '../../assets/checkoutDone.svg'
+
+export function CheckoutDone() {
+  return (
+    <CheckoutDoneContainer>
+      <CheckoutTitle>Uhu! Pedido confirmado</CheckoutTitle>
+      <CheckoutSubtitle>Agora é só aguardar que logo o chegará até você</CheckoutSubtitle>
+
+      <CheckoutDoneMain>
+        <CheckoutOrderInfoWrapper>
+          <CheckoutOrderInfoTypeWrapper>
+            <CheckoutOrderIconWrapper color="#8047F8">
+              <MapPin size={16} color="white" weight="fill" />
+            </CheckoutOrderIconWrapper>
+            <CheckoutOrderSpanWrapper>
+              <CheckoutOrderSpan>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+              </CheckoutOrderSpan>
+              <CheckoutOrderSpan>
+                Farrapos - Porto Alegre - RS
+              </CheckoutOrderSpan>
+            </CheckoutOrderSpanWrapper>
+          </CheckoutOrderInfoTypeWrapper>
+
+          <CheckoutOrderInfoTypeWrapper>
+            <CheckoutOrderIconWrapper color="#DBAC2C">
+              <Timer size={16} color="white" weight="fill" />
+            </CheckoutOrderIconWrapper>
+            <CheckoutOrderSpanWrapper>
+              <CheckoutOrderSpan>Previsão de entrega</CheckoutOrderSpan>
+              <CheckoutOrderSpan bold>20 min - 30 min</CheckoutOrderSpan>
+            </CheckoutOrderSpanWrapper>
+          </CheckoutOrderInfoTypeWrapper>
+
+          <CheckoutOrderInfoTypeWrapper>
+            <CheckoutOrderIconWrapper color="#C47F17">
+              <CurrencyDollar size={16} color="white" weight="fill" />
+            </CheckoutOrderIconWrapper>
+            <CheckoutOrderSpanWrapper>
+              <CheckoutOrderSpan>Pagamento na entrega</CheckoutOrderSpan>
+              <CheckoutOrderSpan bold>Cartão de Crédito</CheckoutOrderSpan>
+            </CheckoutOrderSpanWrapper>
+          </CheckoutOrderInfoTypeWrapper>
+        </CheckoutOrderInfoWrapper>
+        <img src={checkoutDone} alt="Checkout Done Image" />
+      </CheckoutDoneMain>
+    </CheckoutDoneContainer>
+  )
 }
---
+++ src/pages/CheckoutDone/styles.ts
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+
+interface CheckoutIconDivProps {
+  color: string
+}
+
+interface CheckoutOrderSpanProps {
+  bold?: boolean
+}
+
+export const CheckoutDoneContainer = styled.div`
+  margin-top: 80px;
+  line-height: 1.3;
+`
+
+export const CheckoutTitle = styled.h1`
+  font-size: 2rem;
+  color: ${({ theme }) => theme['yellow-700']};
+  font-weight: 800;
+`
+
+export const CheckoutSubtitle = styled.span`
+  font-size: 1.25rem;
+  color: ${({ theme }) => theme['gray-700']};
+`
+
+export const CheckoutDoneMain = styled.div`
+  display: flex;
+  gap: 102px;
+  margin-top: 40px;
+`
+
+export const CheckoutOrderInfoWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 32px;
+  width: 100%;
+  border-radius: 6px 36px;
+  padding: 40px;
+  background-color: ${({ theme }) => theme.background};
+  background-clip: padding-box;
+  border: 1px solid transparent;
+
+  &:before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    margin: -1px;
+    border-radius: inherit;
+    background: linear-gradient(to right, rgba(219, 172, 44, 1), rgba(128, 71, 248, 1));
+    z-index: -1;
+  }
+`
+
+export const CheckoutOrderSpan = styled.span<CheckoutOrderSpanProps>`
+  display: block;
+  font-size: 1rem;
+  color: ${({ theme }) => theme['gray-600']};
+  font-weight: ${({ bold }) => bold ? 'bold' : '400'};
+`
+
+export const CheckoutOrderSpanWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const CheckoutOrderInfoTypeWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`
+
+export const CheckoutOrderIconWrapper = styled.div<CheckoutIconDivProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+  width: 32px;
+  height: 32px;
+  background-color: ${({ color }) => color};
+  border-radius: 50%; 
+`
